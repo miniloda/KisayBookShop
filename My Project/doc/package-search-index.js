2c2aabb87c561903128107f2b66c46146188c59a
miniloda <90799133+miniloda@users.noreply.github.com>
2022-06-04 22:46:06 +0800

    First commit

---
+++ My Project/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.data.datamanagement"},{"l":"com.outline.menu"},{"l":"com.transactions"}];updateSearchResults();
---
+++ My Project/src/com/transactions/DateTimeCheck.java
@@ -0,0 +1,50 @@
+package com.transactions;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;;
+
+/**
+ * Checks for the time
+ * 
+ * @author jacob
+ *
+ */
+public class DateTimeCheck {
+	Date currentDate;
+	SimpleDateFormat timeFormat;
+
+	public DateTimeCheck() {
+		currentDate = new Date();
+
+	}
+
+	/**
+	 * returns current date in format
+	 * 
+	 * @return current date MM/dd/yyyy hh:mm:ss
+	 */
+	public String getDate() {
+		timeFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
+		return timeFormat.format(currentDate);
+	}
+
+	/**
+	 * Generate the Date for the Database
+	 * 
+	 * @return
+	 */
+	public String getDateDB() {
+		timeFormat = new SimpleDateFormat("yyyy-MM-dd");
+		return timeFormat.format(currentDate);
+	}
+
+	/**
+	 * Generate the time for the Database
+	 * 
+	 * @return
+	 */
+	public String getTime() {
+		timeFormat = new SimpleDateFormat("hh:mm:ss");
+		return timeFormat.format(currentDate);
+	}
+}
