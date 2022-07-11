7df311acd0a4001289af27a74a48093283628e41
Kisay <jlusaraga@up.edu.ph>
2022-07-11 08:52:31 +0800

    Delete My Project/doc directory

--- My Project/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jacob.datamanagement"},{"l":"com.jacob.menu"},{"l":"com.jacob.transactions"}];updateSearchResults();
